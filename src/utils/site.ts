cc4998e6254e30b5dc6544513441547b7b1daccf
osushizm <osushi.osushisuki@gmail.com>
2026-01-09 18:04:30 +0900

    めーりう

--- src/utils/site.ts
+++ src/utils/site.ts
@@ -5,7 +5,7 @@ export const siteConfig = {
   tagline: "フルスタックエンジニア / Web開発者",
   description:
     "フルスタックエンジニアの個人サイト。ブログや成果物、技術的な取り組みを紹介しています。",
-  email: "contact@tokisudeniosushi.com",
+  email: "osushi.osushisuki@gmail.com",
   navigation: [
     { href: "/", label: "ホーム" },
     { href: "/blog", label: "ブログ" },
